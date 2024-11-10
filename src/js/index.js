6f04ba0a95af31bee4f79b52dd55571fbb369bc0
SerZeMat <secemacorp@gmail.com>
2024-11-10 23:48:21 +0000

    Simple Counter a falta de poner los botones

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,23 @@ import ReactDOM from "react-dom/client";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SecondsCounter from "./component/secondsCounter.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
+let counter = 0;
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+setInterval(()=>{
+  const numOne = Math.floor(counter / 1);
+  const numTwo = Math.floor(counter / 10);
+  const numThree = Math.floor(counter / 100);
+  const numFour = Math.floor(counter / 1000);
+  const numFive = Math.floor(counter / 10000);
+  const numSix = Math.floor(counter / 100000 )
+counter++;
+
+  root.render(<SecondsCounter digitOne={numOne} digitTwo={numTwo} digitThree={numThree} digitFour={numFour} digitFive={numFive} digitSix={numSix}/>);
+
+},1000);
